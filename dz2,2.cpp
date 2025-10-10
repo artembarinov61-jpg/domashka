5f4a3ee0920b776571ffdf18c8b9043d39b6acc1
artembarinov61-jpg <artembarinov61@gmail.com>
2025-10-11 00:58:11 +0300

    Add files via upload

--- dz2,2.cpp
+++ dz2,2.cpp
@@ -1,36 +1,45 @@
 #include<iostream>
 
 
-
 int main(){
-    setlocale(LC_CTYPE,"rus");
-    //Zadanie 1
-    double a;
-    std::cout<<"Введите число:"<<std::endl;
-    std::cin>> a ;
-    std::cout<< "Вы ввели:\n" << a << std::endl;
+    bool a=true,b=false;
+    std::cout<< "Оператор ||:"<<std::endl;
+    for (int i=0;i<=3;i++){
+       switch (i){
+        case 0:
+            std::cout<< std::boolalpha << a <<"     " << a << "     "<< a||a;
+            break;
+        case 1:
+            std::cout<< "\n"<<std::boolalpha << b <<"     " << a << "     "<<(b||a) ;
+            break;
+        case 2:
+            std::cout<<"\n"<< std::boolalpha << a <<"     " << b << "     "<< a||b ;
+            break;
+        case 3:
+            std::cout<< "\n"<<std::boolalpha << b <<"     " << b << "     "<< b||b;
+            break;
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //Zadanie 2
-    std::string str;
-    std ::cout<< "Введите слово:" << std::endl;
-    std::cin >> str;
-    std::cout <<"Вы ввели:\n" << str << std::endl;
-    
+            }   
+    }
+    std::cout<<"\n\nОператор &&:";
+        for (int i=0;i<=3;i++){
+            switch (i){
+                case 0:
+                    std::cout<< "\n"<< std::boolalpha << a <<"     " << a << "     "<< a&&a;
+                    break;
+                case 1:
+                    std::cout<< "\n"<<std::boolalpha << b <<"     " << a << "     "<<(b&&a) ;
+                    break;
+                case 2:
+                    std::cout<<"\n"<< std::boolalpha << a <<"     " << b << "     "<< a&&b ;
+                    break;
+                case 3:
+                    std::cout<< "\n"<<std::boolalpha << b <<"     " << b << "     "<< b&&b;
+                    break;
 
-
-
-
-
+                }   
+    }
+        
     return 0;
+
 }
---
+++ dz3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+
+int main(){
+    int a,b,c;
+    std::cout<< "ВВедите первое число:";
+    std:: cin>> a;
+    std::cout<<"\nВВедите второе число:";
+    std::cin>>b;
+    std::cout<<"\nВВедите третье число:";
+    std::cin>>c;
+    int max=(a>b && a>c) ? a : b>c ? b: c;
+    std:: cout<< max << "\n";
+    int min=(a<b && a<c)? a: b<c ? b : c;
+    int avg;
+    if ((a>b && a<c)|| (a>c && a<b)){
+        avg=a;
+    }
+    else if((b>a && b<c)|| (b>c && b<a)){
+        avg=b;
+    }
+    else if((c>a && c<b)|| (c>b&& c<a)){
+        avg=c;
+    }
+    std:: cout<< avg <<"\n" << min;
+
+
+
+    return 0;
+}
