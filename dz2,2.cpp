d2739ca13bbe73d14d6641fbc5f054d3c5754586
artembarinov61-jpg <artembarinov61@gmail.com>
2025-10-10 18:47:46 +0300

    Add files via upload

---
+++ dz2,2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+
+
+int main(){
+    //Zadanie 1
+    int a;
+    std::cout<<"ВВедите число :"<<std::endl;
+    std::cin>> a ;
+    std::cout<< "Вы ввели :\n" << a << std::endl;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Zadanie 2
+    std::string str;
+    std ::cout<< "ВВедите слово" << std::endl;
+    std::cin >> str;
+    std::cout <<"Вы ввели:\n" << str << std::endl;
+    
+
+
+
+
+
+    return 0;
+}
