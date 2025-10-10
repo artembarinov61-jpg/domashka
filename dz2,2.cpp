d64139765cd7c6a0b19a719a1995d565872af71d
artembarinov61-jpg <artembarinov61@gmail.com>
2025-10-10 18:56:12 +0300

    Add files via upload

---
+++ dz2,2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+
+
+int main(){
+    setlocale(LC_CTYPE,"rus");
+    //Zadanie 1
+    int a;
+    std::cout<<"ВВедите число :"<<std::endl;
+    std::cin>> a ;
+    std::cout<< "Вы ввели :\n" << a << std::endl;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //Zadanie 2
+    std::string str;
+    std ::cout<< "ВВедите слово" << std::endl;
+    std::cin >> str;
+    std::cout <<"Вы ввели:\n" << str << std::endl;
+    
+
+
+
+
+
+    return 0;
+}
