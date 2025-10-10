8edbf92d7a7583dd2faa20d4678a780b608d9aa5
artembarinov61-jpg <artembarinov61@gmail.com>
2025-10-11 00:54:17 +0300

    Update dz2,2.cpp

--- dz2,2.cpp
+++ dz2,2.cpp
@@ -6,9 +6,9 @@ int main(){
     setlocale(LC_CTYPE,"rus");
     //Zadanie 1
     double a;
-    std::cout<<"ВВедите число :"<<std::endl;
+    std::cout<<"Введите число:"<<std::endl;
     std::cin>> a ;
-    std::cout<< "Вы ввели :\n" << a << std::endl;
+    std::cout<< "Вы ввели:\n" << a << std::endl;
 
     
     
@@ -23,7 +23,7 @@ int main(){
     
     //Zadanie 2
     std::string str;
-    std ::cout<< "ВВедите слово" << std::endl;
+    std ::cout<< "Введите слово:" << std::endl;
     std::cin >> str;
     std::cout <<"Вы ввели:\n" << str << std::endl;
     
