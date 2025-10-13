0a5c772db5e9575924e1d9f4daf98df44b846b60
artembarinov61-jpg <artembarinov61@gmail.com>
2025-10-13 18:49:50 +0300

    Add files via upload

---
+++ dz2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+
+int main(){
+    std::string a;
+    std::cout<<"Введите число:"<<std::endl;
+    std::cin>> a ;
+    std::cout<< "Вы ввели:\n" << a << std::endl;
+
+
+    std::string slovo;
+    std::cout<<"Введите слово:"<<std::endl;
+    std::cin>> slovo;
+    std::cout<<"Вы ввели:\n"<<slovo<<std::endl;
+
+    
+    
+    
+    
+    
+    return 0;
+}
