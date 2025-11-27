afd6171a50646383cbfbe5bc04323d2862ada823
artembarinov61-jpg <artembarinov61@gmail.com>
2025-11-27 18:39:51 +0300

    Update dz28.cpp

--- dz28.cpp
+++ dz28.cpp
@@ -6,9 +6,14 @@
 int main(int argc,char** argv){
     std::string str1;
     std::ifstream in("//Users//artembarinov61icloud.com//Desktop//zxc//in.txt");
-    while (!(in>>str1).eof()){
+    if (in.is_open()){
+    while (in>>str1){
         std::cout<<str1<<std::endl;
+    }else{
+        std::cout << "Ошибка! Файл не найден.\n";
+        return 1;
     }
+    
     in.close();
 
 
